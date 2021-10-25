874ecbc199e36dae6bce5768941c9511e64fcf8b
Ludo Tielbeke <97067883@st.deltion.nl>
2021-10-25 12:59:57 +0200

    added itemtoevoegen

---
+++ Hoofstuk 8/index.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['artikelen'])) {
+    $artikelen = array(
+        array('artikel' => "pizza Margherita", "prijs" => "12,50", "aantal" => 0),
+        array('artikel' => "Pizza Funghi", "prijs" => "12,50", "aantal" => 0),
+        array('artikel' => "Pizza Marina", "prijs" => "13,95", "aantal" => 0),
+        array('artikel' => "Pizza Hawai", "prijs" => "11,30", "aantal" => 0),
+        array('artikel' => "Pizza Quattro Formaggi", "prijs" => "14,50", "aantal" => 0)
+    );
+    $_SESSION['artikelen'] = $artikelen;
+}
+
+$artikelen = $_SESSION['artikelen'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizza di mama</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <header>
+        <div class="jumbotron text-center">
+            <h1>Pizza di mama</h1>
+            <a href="winkelwagen.php">
+                <button type="button" class="btn btn-primary">Winkelwagen</button>
+            </a>
+        </div>
+    </header>
+    <section>
+        <?php
+        function printArtikelen($item, $key)
+        {
+            echo '
+                <div class="col-sm-4">
+                <h3>' . $item['artikel'] . '</h3>
+                <p>geserveerd met heerlijke kaas en extra veel tomatensaus</p>
+                <p> &euro; ' . $item['prijs'] . '</p>
+                <form action="itemToevoegen.php" method="post">
+                <button type="submit" name="artikelNummer" value="'.$key.'" class="btn btn-success">Toevoegen '.$item['aantal'].'</button>
+                </form>
+                </div>
+            ';
+        }
+        array_walk($artikelen, 'printArtikelen');
+        ?>
+    </section>
+    <footer></footer>
+</body>
+
+</html>
---
+++ Hoofstuk 8/itemToevoegen.php
@@ -0,0 +1,9 @@
+<?php
+
+session_start();
+$artikelen = $_SESSION['artikelen'];
+$toevoegen = $_POST['artikelNummer'];
+$artikelen[$toevoegen]["aantal"] += 1;
+$_SESSION["artikelen"] = $artikelen;
+header('Location: index.php');
+?>
